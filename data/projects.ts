26a2f634fcd5fff67fcd2d966e2547c87581ce32
Mohan <84113216+mohan08sin@users.noreply.github.com>
2025-05-11 19:19:50 +0530

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -18,19 +18,19 @@ export const projects: Project[] = [
   {
     title: 'VSCode Portfolio',
     description:
-      'A Visual Studio Code themed developer portfolio built with Next.js.',
+      'A Visual Studio Code themed developer portfolio, built with Next.js.',
     logo: '/logos/vsc.svg',
     link: 'https://github.com/mohan08sin/Portfolio_vs',
     slug: 'vscode-portfolio',
   },
-  // {
-  //   title: 'Subtrackt',
-  //   description:
-  //     'A simple and elegant way to track your subscriptions and save money.',
-  //   logo: '/logos/subtrackt.svg',
-  //   link: 'https://github.com/',
-  //   slug: 'subtrackt',
-  // },
+  {
+    title: 'Employee Management',
+    description:
+      'A full-stack Employee Management System with CRUD operations, built using React, Spring Boot, and MySQL.',
+    logo: '/logos/react_icon.svg',
+    link: 'https://github.com/mohan08sin/Employee-Management',
+    slug: 'Employee Management',
+  },
   // {
   //   title: 'Coolify Deployments',
   //   description:
